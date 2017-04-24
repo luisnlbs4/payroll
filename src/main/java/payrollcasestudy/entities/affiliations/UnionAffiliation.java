45817061e01ca84fb23ffa4ce484d273f54ae100
César Ivan Delgado Silva <ceides9497@gmail.com>
2017-04-23 20:32:43 -0400

    Tratando de arreglar failures

--- src/main/java/payrollcasestudy/entities/affiliations/UnionAffiliation.java
+++ src/main/java/payrollcasestudy/entities/affiliations/UnionAffiliation.java
@@ -1,14 +1,15 @@
 package payrollcasestudy.entities.affiliations;
 
 import java.util.Calendar;
-
+import java.util.HashMap;
+import java.util.Map;
 
 import payrollcasestudy.entities.ServiceCharge;
 
 public class UnionAffiliation {
 
-	public static final UnionAffiliation NO_AFFILIATION = new UnionAffiliation(0,0);
-	private ServiceCharge servicecharge = new ServiceCharge(null, 0.0);
+	public static final UnionAffiliation NO_AFFILIATION = new UnionAffiliation(-1,0);
+	Map<Calendar, ServiceCharge> serviceCharges = new HashMap<Calendar, ServiceCharge>();
 	private int memberID;
 	private double amount;
 	
@@ -20,7 +21,11 @@ public class UnionAffiliation {
 
 	public ServiceCharge getServiceCharge(Calendar date) {
 		// TODO Auto-generated method stub
-		return servicecharge;
+		return serviceCharges.get(date);
+	}
+	
+	public void addServiceCharge(Calendar date, double amount) {
+		this.serviceCharges.put(date, new ServiceCharge(date, amount));
 	}
 
 	public Double getDues() {
