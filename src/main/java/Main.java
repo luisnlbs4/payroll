c923c2e1510c30188035cde97be851ac88900d8b
César Ivan Delgado Silva <ceides9497@gmail.com>
2017-05-07 23:04:16 -0400

    Modificaciones
    -se elimino lo que no se usa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,33 +22,14 @@ public class Main {
 			 		      return new ModelAndView(new HashMap(), "templates/newEmployee.vm");
 			 		    }, new VelocityTemplateEngine());
 		
-		get("/NewEmployeeComisioned", (request, response) -> {
-		      return new ModelAndView(new HashMap(), "templates/newEmployeeComisioned.vm");
-		    }, new VelocityTemplateEngine());
-		
-		get("/NewEmployeeByHours", (request, response) -> {
-		      return new ModelAndView(new HashMap(), "templates/newEmployeeByHours.vm");
-		    }, new VelocityTemplateEngine());
 		
 		post("/newEmployee", (request, response) -> EmployeePresenter.registrar_empleado(request.queryParams("kind"),request.queryParams("nombre"),request.queryParams("direccion"),request.queryParams("ci"), request.queryParams("salario"),request.queryParams("comision")));
-		
-		/*post("/newEmployee", (request, response) -> EmployeePresenter.registrar_empleado_Asalariado(request.queryParams("nombre"),request.queryParams("direccion"),request.queryParams("ci"), request.queryParams("salario")));
-		post("/newEmployeeComisioned", (request, response) -> EmployeePresenter.registrar_empleado_Asalariado_con_Comision(request.queryParams("nombre"),request.queryParams("direccion"),request.queryParams("ci"), request.queryParams("salario"),request.queryParams("comision")));
-		post("/newEmployeeByHours", (request, response) -> EmployeePresenter.registrar_empleado_Asalariado(request.queryParams("nombre"),request.queryParams("direccion"),request.queryParams("ci"), request.queryParams("salario")));
-		*/
+
 		HashMap<String,Object> view = new HashMap<String, Object>();
 		get("/Employees", (request, response) -> {
 			view.put("employees", EmployeePresenter.Devolver_empleados());
 		      return new ModelAndView(view, "templates/indexEmployee.vm");
 		    }, new VelocityTemplateEngine());
-		
-		
 
-		//get("/", (request, response) -> Index.idexView());
-	
-		/*get("/createNewEmployee", (request, response) -> EmployeeView.registrationForm());
-		post("/newEmployee", (request, response) -> EmployeeView.createNewEmployee(request.queryParams("id"), request.queryParams("name"), request.queryParams("address")));
-		get("/showEmployee", (request, response) -> EmployeeView.showEmployee());
-		get("/showAllEmployees", (request, response) -> EmployeeView.showAllEmployees());*/
 	}
 }
