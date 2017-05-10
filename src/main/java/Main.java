20b32bcd0087d5871b63d0eda623da450a9e9274
Luis Veizaga <luisnlbs4@gmail.com>
2017-05-10 06:21:50 -0400

    Agregando horas a pago por horas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import payrollcasestudy.entities.Employee;
+import payrollcasestudy.entities.PayCheck;
 import Presenters.EmployeePresenter;
+import Presenters.PaymentPresenter;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
@@ -14,34 +16,49 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		HashMap<String,Object> view = new HashMap<String, Object>();
-
-		
+		HashMap<String,Object> view = new HashMap<String, Object>();	
 		get("/", (request, response) -> {
 		      return new ModelAndView(view, "templates/welcome/index.vtl");
 		    }, new VelocityTemplateEngine());
 		
 		
-		
-		
 		get("/NewEmployee", (request, response) -> {
 			 		      return new ModelAndView(view, "templates/employee/newEmployee.vtl");
-			 		    }, new VelocityTemplateEngine());		
+			 		    }, new VelocityTemplateEngine());					
+		get("/Employees", (request, response) -> {
+			view.put("employees", EmployeePresenter.Devolver_empleados());
+		      return new ModelAndView(view, "templates/employee/indexEmployee.vtl");
+		    }, new VelocityTemplateEngine());		
 		post("/newEmployee", (request, response) -> {		
 			 EmployeePresenter.registrar_empleado(request.queryParams("kind"),request.queryParams("nombre"),request.queryParams("direccion"),request.queryParams("ci"), request.queryParams("salario"),request.queryParams("comision"));
 			 response.redirect("/Employees");
 	         return new ModelAndView(view, "templates/employee/indexEmployee.vtl");
 		}, new VelocityTemplateEngine());
-			
-		get("/Employees", (request, response) -> {
-			view.put("employees", EmployeePresenter.Devolver_empleados());
-		      return new ModelAndView(view, "templates/employee/indexEmployee.vtl");
-		    }, new VelocityTemplateEngine());		
+		
+		
+		
 		get("/EmployeeAddHours/:id", (request, response) -> {			
 			Employee employee = EmployeePresenter.getEmployee(Integer.parseInt(request.params(":id")));
 			view.put("employee", employee);
-			  return new ModelAndView(view, "templates/employee/employeeAddhours.vtl");
+			  return new ModelAndView(view, "templates/payment/employeeAddhours.vtl");
 			}, new VelocityTemplateEngine());
+		
+		post("/payHourly", (request, response) -> {			 			
+ 			PaymentPresenter.createPayPerHour(request.queryParams("year"),request.queryParams("month"),request.queryParams("day"), request.queryParams("hours"),request.queryParams("employeeId"));
+ 			response.redirect("/Employees");
+            return new ModelAndView(view, "templates/employee/indexEmployee.vtl");
+         }, new VelocityTemplateEngine());
+		
+		get("/pay/show/:id", (request, response) -> {
+			
+			Employee employee = EmployeePresenter.getEmployee(Integer.parseInt(request.params(":id")));
+			PayCheck payCheck = PaymentPresenter.getPayCheckFromPayDayTransaction((request.params(":id")));
+			double SalarioTotal = payCheck.getNetPay();
+			view.put("employee", employee);
+			view.put("salary", SalarioTotal);
+            return new ModelAndView(view, "templates/payment/payEmployee.vtl");
+        }, new VelocityTemplateEngine());
+		
 
 	}
 }
--- src/main/java/Presenters/PaymentPresenter.java
+++ src/main/java/Presenters/PaymentPresenter.java
@@ -1,6 +1,37 @@
 package Presenters;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import payrollcasestudy.entities.PayCheck;
+import payrollcasestudy.transactions.PaydayTransaction;
+import payrollcasestudy.transactions.Transaction;
+import payrollcasestudy.transactions.add.AddTimeCardTransaction;
+
 public class PaymentPresenter {
+	
+	private static Transaction paymentTransaction;
+	private static PaydayTransaction paydayTransaction;
+
+	
+	public static void createPayPerHour(String year, String month, String day, String hours, String employeeId)
+	{
+		Calendar date = new GregorianCalendar(Integer.parseInt(year),Integer.parseInt(month)-1,Integer.parseInt(day));
+		paymentTransaction = new AddTimeCardTransaction(date, Double.parseDouble(hours),Integer.parseInt(employeeId));
+		paymentTransaction.execute();
+	}
+	
+	public static void calculateAllPays(String year, String month, String day)
+	{
+		Calendar date = new GregorianCalendar(Integer.parseInt(year),Integer.parseInt(month),Integer.parseInt(day));
+	    paydayTransaction = new PaydayTransaction(date);
+		paydayTransaction.execute();		
+	}
+	
+	public static PayCheck getPayCheckFromPayDayTransaction(String employeeId)
+	{
+		return paydayTransaction.getPaycheck(Integer.parseInt(employeeId));
+	}
 	
 
 }
