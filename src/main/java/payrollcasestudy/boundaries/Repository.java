574a8b1f5a1e80b695b79b8ad8bc83920ac6272c
Luis Veizaga <luisnlbs4@gmail.com>
2017-05-21 18:43:35 -0400

    agregando Repository

---
+++ src/main/java/payrollcasestudy/boundaries/Repository.java
@@ -0,0 +1,20 @@
+package payrollcasestudy.boundaries;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import payrollcasestudy.entities.Employee;
+
+public interface Repository {
+	    public Employee getEmployee(int employeeId) ;
+	    public void addEmployee(int employeeId, Employee employee);
+	    public void clear();
+	    public void deleteEmployee(int employeeId);
+	    public Employee getUnionMember(int memberId);
+	    public void addUnionMember(int memberId, Employee employee);
+	    public void deleteUnionMember(int memberId);
+	    public Set<Integer> getAllEmployeeIds() ;
+	    public ArrayList<Employee> getAllEmployees(); 
+
+}
